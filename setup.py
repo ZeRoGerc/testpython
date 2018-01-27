def913d187c73ddd60d15437ade00aaf8d8161ca
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-01-27 12:56:02 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ttesttpython',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/ZeRoGerc/testpython',
+    license='Apache 2.0',
+    author='zerogerc',
+    author_email='zerogerc@gmail.com',
+    description='Left pad function for python'
+)
